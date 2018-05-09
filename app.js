756409329c46aa506ed56868816b078e835ff1aa
Jonathan <jonathanbachmann@gmail.com>
2018-05-08 20:33:22 -0600

    First functioning angular app of Mean Hotel
    The main sample app that was being built in this Udemy
    tutorial used Mongo, express, and node but not angular
    The source checked in for this commit now includes a
    first working implementation using Angular
    
    Using nodemon in npm start

--- app.js
+++ app.js
@@ -7,14 +7,25 @@ var bodyParser = require('body-parser');
 
 app.set('port', 3000);
 
+// Middleware to console log every request
 app.use(function(req, res, next) {
     console.log(req.method, req.url);
     next();
 });
+
+// Static directory, set before defining routes
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Mount node modules middleware
+app.use('/node_modules', express.static(__dirname + '/node_modules'))
+
+// Enable parsing of posted forms
 app.use(bodyParser.urlencoded({"extended": false}));
+
+// Set up routing
 app.use('/api', routes);
 
+// Start listening for requests
 var server = app.listen(app.get('port'), function() {
     var port = server.address().port;
     console.log('Magic happens on port ' + port);
---
+++ public/angular-app/hotel-list/hotel-list-controller.js
@@ -0,0 +1,10 @@
+angular.module('meanhotel').controller('HotelsController', HotelsController)
+
+function HotelsController(hotelDataFactory) {
+    var vm = this;
+    vm.title = 'MEAN Hotel App';
+    hotelDataFactory.hotelList().then(function(response) {
+        vm.hotels = response.data;
+        console.log(vm.hotels);
+    });
+}
